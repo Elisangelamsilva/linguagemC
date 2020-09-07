0123fbf12d4705848402c514cdba07c3162b9fc4
Elisangela <elisangela00silva@gmail.com>
2020-09-07 16:11:07 -0300

    Create 44VetoresEstruturas.c

---
+++ 44VetoresEstruturas.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (void){
+    struct horario
+    {
+        int hora;
+        int minuto;
+        int segundo;
+    };
+
+    struct horario teste[5];
+    //m vetor do tipo horario que contém 5x as váriaveis da estrutura 
+    
+    
+}
