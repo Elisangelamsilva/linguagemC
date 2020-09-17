163f66f7ec02c560fc3e369015942f25a730c51a
Elisangela <elisangela00silva@gmail.com>
2020-09-17 12:13:26 -0300

    exercicio sobre numeros quadrados, cubos e raiz

--- exercicioDeRepeticao/16.c
+++ exercicioDeRepeticao/16.c
@@ -9,6 +9,42 @@
 #include <math.h>
 #include <stdarg.h>
 
+int quantValoresObtido;
+float quadrado =0;
+float cubo =0;
+double raiz;
+
 int main(){
-    
+
+    printf("Quantos valores:\n");
+    scanf("%i", &quantValoresObtido);
+
+    int valoresUsuario[quantValoresObtido];
+
+    int obtendoValores(int valoresUsuario[]);
+    obtendoValores(valoresUsuario);
+
+    int realizarCalculo(int valoresUsuario[]);
+    realizarCalculo(valoresUsuario);
+
+
+}
+int obtendoValores(int valoresUsuario[]){
+
+    printf("Insira os valores:\n");
+    for(int i =0; i < quantValoresObtido; i++){
+        scanf("%i", &valoresUsuario[i]);
+    }
+}
+int realizarCalculo(int valoresUsuario[]){
+    int contador;
+
+    for(int i = 0;i<quantValoresObtido;i++){
+        quadrado = valoresUsuario[i] * valoresUsuario[i];
+        cubo = valoresUsuario[i] * valoresUsuario[i] * valoresUsuario[i]; 
+        raiz = sqrt(valoresUsuario[i]);
+
+        printf("entrou sim %i quadrado: %f cubo: %f raiz quadrada: %f\n", valoresUsuario[i], quadrado, cubo, raiz);
+    }
+
 }
