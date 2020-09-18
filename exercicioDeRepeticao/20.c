703c73f05be3b272aa49b307c2040432837672ae
Elisangela <elisangela00silva@gmail.com>
2020-09-18 15:08:53 -0300

    aula 53 sobre a função getchar em strings

---
+++ exercicioDeRepeticao/20.c
@@ -0,0 +1,28 @@
+// 20. Faça um algoritmo que leia vários números inteiros e calcule o somatório dos números negativos. O fim da leitura será indicado pelo número 0.
+
+#include <stdio.h>
+#include <string.h>
+#include <complex.h> 
+#include <fenv.h>
+#include <float.h> 
+#include <limits.h> 
+#include <math.h>
+#include <stdarg.h>
+
+int main(){
+
+    printf("Insira alguns números:\n");
+    scanf("");
+
+    int numerosObtidos[];
+
+    while(numerosObtidos ){
+        /* code */
+    }
+    
+
+
+
+
+
+}
