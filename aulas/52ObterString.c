8e813eada48d85ff9a40ed5c13ba4984be508dd1
Elisangela <elisangela00silva@gmail.com>
2020-09-18 09:08:21 -0300

    aula 52, como obter strings utilizando uma função

---
+++ aulas/52ObterString.c
@@ -0,0 +1,13 @@
+//quando é necessario poucos dados
+#include <stdio.h>
+
+int main(void){
+
+    char nome[20];
+    char sobrenome[20];
+
+    printf("Insira seu nome e sobrenome:\n");
+    scanf("%s%s", &nome,&sobrenome);
+
+    printf("%s %s\n", nome, sobrenome);
+}
